752442ca156f3011040ca5193d7a7e862584794e
lyudmylaz <lyudmyla_zahar@gmail.com>
2020-06-07 00:13:24 +0200

    improvement to model

--- main.py
+++ main.py
@@ -3,78 +3,86 @@ import cv2
 import torch
 import os
 from torchvision.models import resnet18
+from time import time
+from sklearn import tree
+from sklearn import ensemble
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+N_sample = 700
+p = 0.08
 embedding_size = 25
-img_sizes = (1000,1000,3)
-img1 = cv2.imread("neutral_front/003_03.jpg")
-img1 = np.resize(img1, img_sizes)
-img2 = cv2.imread("smiling_front/003_08.jpg")
-img2 = np.resize(img2, img_sizes)
-img3 = cv2.imread("neutral_front/009_03.jpg")
-img2 = np.resize(img3, img_sizes)
-
+img_sizes = (1000,1000)
+# img1 = cv2.imread("neutral_front/003_03.jpg")
+# # img1 = np.resize(img1, img_sizes)
+# img = cv2.resize(img1, (1000,1000))
+# img2 = cv2.imread("smiling_front/003_08.jpg")
+# # img2 = np.resize(img2, img_sizes)
+# img3 = cv2.imread("neutral_front/009_03.jpg")
+# # img2 = np.resize(img3, img_sizes)
 model = resnet18(pretrained=True)
-output1 = model(torch.Tensor(img1).unsqueeze(0).permute(0,3,1,2)).detach().numpy()
-output2 = model(torch.Tensor(img2).unsqueeze(0).permute(0,3,1,2)).detach().numpy()
-output3 = model(torch.Tensor(img3).unsqueeze(0).permute(0,3,1,2)).detach().numpy()
-print(((output1-output2)**2).sum(), ((output1-output3)**2).sum())
 
-class DynamicNet(torch.nn.Module):
-    def __init__(self, D_in, H, D_out):
-        """
-        In the constructor we construct three nn.Linear instances that we will use
-        in the forward pass.
-        """
-        super(DynamicNet, self).__init__()
-        self.input_linear = torch.nn.Linear(D_in, H)
-        self.middle_linear = torch.nn.Linear(H, H)
-        self.output_linear = torch.nn.Linear(H, D_out)
+def get_embed(img, model = model):
+    '''get resNet mapping of size 1000 for each image'''
+    return model(torch.Tensor(img).unsqueeze(0).permute(0,3,1,2)).detach().numpy()
 
-    def forward(self, x):
-        """
-        For the forward pass of the model, we randomly choose either 0, 1, 2, or 3
-        and reuse the middle_linear Module that many times to compute hidden layer
-        representations.
+def read_image(path, new_shape = img_sizes):
+    '''read and reshape the image'''
+    img = cv2.imread(path)
+    return cv2.resize(img, new_shape)
 
-        Since each forward pass builds a dynamic computation graph, we can use normal
-        Python control-flow operators like loops or conditional statements when
-        defining the forward pass of the model.
+start  = time()
+lst = os.listdir("neutral_front")
+ids = [x[:3] for x in lst if len(x)==10]
+photos_by_id = {x:dict({"neutral":get_embed(read_image("neutral_front/"+x+"_03.jpg")),
+                        "smiling":get_embed(read_image("smiling_front/"+x+"_08.jpg"))}) for x in ids}
+sample_generator = np.random.choice([False, True], N_sample, p = (1-p,p))
+train_ids = ids[:90].copy()
+test_ids = ids[90:].copy()
+emb_diff = []
+for i in list(sample_generator):
+    if i:
+        id1, id2 = np.random.choice(train_ids, 2)
+        emb_diff.append(np.abs(photos_by_id[id1]["neutral"] - photos_by_id[id2]["smiling"]))
+    else:
+        id1 = np.random.choice(train_ids)
+        emb_diff.append(np.abs(photos_by_id[id1]["neutral"] - photos_by_id[id1]["smiling"]))
 
-        Here we also see that it is perfectly safe to reuse the same Module many
-        times when defining a computational graph. This is a big improvement from Lua
-        Torch, where each Module could be used only once.
-        """
-        h_relu = self.input_linear(x).clamp(min=0)
-        for _ in range(random.randint(0, 3)):
-            h_relu = self.middle_linear(h_relu).clamp(min=0)
-        y_pred = self.output_linear(h_relu)
-        return y_pred
+data_x_train = np.array(emb_diff)[:,0,:]**2
+data_y_train = sample_generator.astype(int)
+#
+# clf  =tree.DecisionTreeClassifier(criterion="entropy", max_depth=8, min_samples_leaf=10)
+# clf.fit(data_x_train, data_y_train)
+
+rf = ensemble.RandomForestClassifier(n_estimators=500,class_weight={0: 1, 1: 100}, max_features=10)
+rf.fit(data_x_train, data_y_train)
+sample_generator_test = np.random.choice([False, True], 300, p = (1-p,p))
+emb_diff = []
+for i in list(sample_generator_test):
+    if i:
+        id1, id2 = np.random.choice(test_ids, 2)
+        emb_diff.append(np.abs(photos_by_id[id1]["neutral"] - photos_by_id[id2]["smiling"]))
+    else:
+        id1 = np.random.choice(test_ids)
+        emb_diff.append(np.abs(photos_by_id[id1]["neutral"] - photos_by_id[id1]["smiling"]))
+
+data_x_test = np.array(emb_diff)[:,0,:]**2
+data_y_test = sample_generator_test.astype(int)
+# print(clf.score(data_x_train, data_y_train), clf.score(data_x_test, data_y_test))
+print(rf.score(data_x_train, data_y_train), rf.score(data_x_test, data_y_test))
+
+allowed_ids = ids[:45] + ids[90:95]
+actual_res = np.zeros(len(ids))
+actual_res[:45] = 1
+actual_res[90:95] = 1
+predicted = np.zeros(len(ids))
+
+for i in range(len(ids)):
+    current = photos_by_id[ids[i]]["smiling"]
+    for id in allowed_ids:
+        checked = photos_by_id[id]["neutral"]
+        if rf.predict((checked-current)**2)==1:
+            predicted[i] = 1
+            break
 
 
-# N is batch size; D_in is input dimension;
-# H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 64, 1000, 100, 10
-
-# Create random Tensors to hold inputs and outputs
-x = torch.randn(N, D_in)
-y = torch.randn(N, D_out)
-
-# Construct our model by instantiating the class defined above
-model = DynamicNet(D_in, H, D_out)
-
-# Construct our loss function and an Optimizer. Training this strange model with
-# vanilla stochastic gradient descent is tough, so we use momentum
-criterion = torch.nn.MSELoss(reduction='sum')
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, momentum=0.9)
-for t in range(500):
-    # Forward pass: Compute predicted y by passing x to the model
-    y_pred = model(x)
-
-    # Compute and print loss
-    loss = criterion(y_pred, y)
-    if t % 100 == 99:
-        print(t, loss.item())
-
-    # Zero gradients, perform a backward pass, and update the weights.
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+confusion_matrix(actual_res, predicted)
