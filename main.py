d9bde7e375acc25f2320150c166539b76e948e72
lyudmylaz <lyudmyla_zahar@gmail.com>
2020-06-09 17:52:33 +0200

    biometric final version - working 70%

--- main.py
+++ main.py
@@ -13,16 +13,18 @@ import copy
 import skimage
 from skimage.morphology import convex_hull_image, erosion
 from skimage.morphology import square
-
+import torchvision
 N_sample = 700
 p = 0.2
 embedding_size = 25
 img_sizes = (1000, 1000)
 model = resnet18(pretrained=True)
-
-
+model2 = torchvision.models.detection.keypointrcnn_resnet50_fpn(pretrained=True)
+model2.eval()
 def get_embed(img, model=model):
     '''get resNet mapping of size 1000 for each image'''
+    if len(img.shape)==2:
+        img = torch.Tensor(img).unsqueeze(2)
     return model(torch.Tensor(img).unsqueeze(0).permute(0, 3, 1, 2)).detach().numpy()
 
 
@@ -64,79 +66,79 @@ start = time()
 lst = os.listdir("neutral_front")
 ids = [x[:3] for x in lst if len(x) == 10]
 #
-
-# use resnet for each photo
-photos_by_id = {x: dict({"neutral": get_embed(read_image("neutral_front/" + x + "_03.jpg")),
-                         "smiling": get_embed(read_image("smiling_front/" + x + "_08.jpg"))}) for x in ids}
+#
+# # use resnet for each photo
+# photos_by_id = {x: dict({"neutral": get_embed(read_image("neutral_front/" + x + "_03.jpg")),
+#                          "smiling": get_embed(read_image("smiling_front/" + x + "_08.jpg"))}) for x in ids}
 #
 
 # generate set of pairs neutral + smiling
-sample_generator = np.random.choice([False, True], N_sample, p=(p, 1 - p))
-train_ids = ids[:90].copy()
-test_ids = ids[90:].copy()
-emb_diff = []
-for i in list(sample_generator):
-    if i:
-        id1, id2 = np.random.choice(train_ids, 2)
-        emb_diff.append(np.abs(photos_by_id[id1]["neutral"] - photos_by_id[id2]["smiling"]))
-    else:
-        id1 = np.random.choice(train_ids)
-        emb_diff.append(np.abs(photos_by_id[id1]["neutral"] - photos_by_id[id1]["smiling"]))
-
-data_x_train = np.array(emb_diff)[:, 0, :] ** 2
-data_y_train = sample_generator.astype(int)
+# sample_generator = np.random.choice([False, True], N_sample, p=(p, 1 - p))
+# train_ids = ids[:90].copy()
+# test_ids = ids[90:].copy()
+# emb_diff = []
+# for i in list(sample_generator):
+#     if i:
+#         id1, id2 = np.random.choice(train_ids, 2)
+#         emb_diff.append(np.abs(photos_by_id[id1]["neutral"] - photos_by_id[id2]["smiling"]))
+#     else:
+#         id1 = np.random.choice(train_ids)
+#         emb_diff.append(np.abs(photos_by_id[id1]["neutral"] - photos_by_id[id1]["smiling"]))
+#
+# data_x_train = np.array(emb_diff)[:, 0, :] ** 2
+# data_y_train = sample_generator.astype(int)
 #
 
-# generate table with correct faces: neutral == smiling
-ids_table = []
-for id in ids:
-    diff = np.abs(photos_by_id[id]["neutral"] - photos_by_id[id]["smiling"]) ** 2
-    ids_table.append(diff)
-
-ids_table = np.array(ids_table)[:, 0, :]
+# # generate table with correct faces: neutral == smiling
+# ids_table = []
+# for id in ids:
+#     diff = np.abs(photos_by_id[id]["neutral"] - photos_by_id[id]["smiling"]) ** 2
+#     ids_table.append(diff)
 #
-
-# random forest model
-rf = ensemble.RandomForestClassifier(n_estimators=500, class_weight={0: 1, 1: 1}, max_features=10)
-rf.fit(data_x_train, data_y_train)
+# ids_table = np.array(ids_table)[:, 0, :]
+# #
 #
+# # random forest model
+# rf = ensemble.RandomForestClassifier(n_estimators=500, class_weight={0: 1, 1: 1}, max_features=10)
+# rf.fit(data_x_train, data_y_train)
+# #
 
 # test model
-sample_generator_test = np.random.choice([False, True], 300, p=(p, 1 - p))
-print(rf.predict(ids_table).mean())
-
-emb_diff = []
-for i in list(sample_generator_test):
-    if i:
-        id1, id2 = np.random.choice(test_ids, 2)
-        emb_diff.append(np.abs(photos_by_id[id1]["neutral"] - photos_by_id[id2]["smiling"]))
-    else:
-        id1 = np.random.choice(test_ids)
-        emb_diff.append(np.abs(photos_by_id[id1]["neutral"] - photos_by_id[id1]["smiling"]))
-
-data_x_test = np.array(emb_diff)[:, 0, :] ** 2
-data_y_test = sample_generator_test.astype(int)
-print(rf.score(data_x_train, data_y_train), rf.score(data_x_test, data_y_test))
+# sample_generator_test = np.random.choice([False, True], 300, p=(p, 1 - p))
+# print(rf.predict(ids_table).mean())
 #
-
-# calculate result table
-allowed_ids = ids[:45] + ids[90:95]
-actual_res = np.ones(len(ids))
-actual_res[:45] = 0
-actual_res[90:95] = 0
-predicted = np.ones(len(ids))
-
-for i in range(len(ids)):
-    current = photos_by_id[ids[i]]["smiling"]
-    print(i)
-    for id in allowed_ids:
-        checked = photos_by_id[id]["neutral"]
-        if rf.predict((checked - current) ** 2) == 0:
-            predicted[i] = 0
-            break
-
-confusion_matrix(actual_res, predicted)
+# emb_diff = []
+# for i in list(sample_generator_test):
+#     if i:
+#         id1, id2 = np.random.choice(test_ids, 2)
+#         emb_diff.append(np.abs(photos_by_id[id1]["neutral"] - photos_by_id[id2]["smiling"]))
+#     else:
+#         id1 = np.random.choice(test_ids)
+#         emb_diff.append(np.abs(photos_by_id[id1]["neutral"] - photos_by_id[id1]["smiling"]))
 #
+# data_x_test = np.array(emb_diff)[:, 0, :] ** 2
+# data_y_test = sample_generator_test.astype(int)
+# print(rf.score(data_x_train, data_y_train), rf.score(data_x_test, data_y_test))
+# #
+
+# # calculate result table
+# allowed_ids = ids[:45] + ids[90:95]
+# actual_res = np.ones(len(ids))
+# actual_res[:45] = 0
+# actual_res[90:95] = 0
+# predicted = np.ones(len(ids))
+#
+# for i in range(len(ids)):
+#     current = photos_by_id[ids[i]]["smiling"]
+#     print(i)
+#     for id in allowed_ids:
+#         checked = photos_by_id[id]["neutral"]
+#         if rf.predict((checked - current) ** 2) == 0:
+#             predicted[i] = 0
+#             break
+#
+# confusion_matrix(actual_res, predicted)
+# #
 
 
 face_cascade = cv2.CascadeClassifier(os.path.join(cv2.haarcascades, 'haarcascade_frontalface_default.xml'))
@@ -145,9 +147,25 @@ eye_cascade = cv2.CascadeClassifier(os.path.join(cv2.haarcascades, 'haarcascade_
 smile_faces_detection = {id: select_face("smiling_front/" + id + "_08.jpg") for id in ids}
 neutral_faces_detection = {id: select_face("neutral_front/" + id + "_03.jpg") for id in ids}
 
+
+
+test = {id: read_image("smiling_front/" + id + "_08.jpg") for id in ids}
+m1 = model2([torch.Tensor(test["001"]).permute(2, 0, 1)/255.0])
+img = (test["001"])
+
+radius = 3
+color = (255, 0, 0)
+thickness = 2
+img2 = img.copy()
+for x,y,z in m1[0]["keypoints"][0][:5]:
+    img2 = cv2.circle(img2, (x,y), radius, color, thickness)
+
+# Displaying the image
+plt.imshow(img2)
+
 # use resnet for each photo
-photos_by_id = {x: dict({"neutral": get_embed(neutral_faces_detection[id]),
-                         "smiling": get_embed(smile_faces_detection[id])}) for x in ids}
+photos_by_id = {id: dict({"neutral": get_embed(neutral_faces_detection[id]),
+                         "smiling": get_embed(smile_faces_detection[id])}) for id in ids}
 #
 
 # generate set of pairs neutral + smiling
